3562f079238300e88c21c35f3c00f155ab8760d4
Martin Lacayo <mlacayoemery@gmail.com>
2017-06-13 15:25:28 +0200

    Added PyWPS4 demo to InVEST WPS server script.

--- tools/invest/wps.py
+++ tools/invest/wps.py
@@ -131,5 +131,124 @@ def parse_tool():
     # or for a file-like stream:
     #template = pkg_resources.resource_stream(resource_package, resource_path)
 
+import os
+import flask
+
+import pywps
+from pywps import Service
+
+from processes.sleep import Sleep
+from processes.ultimate_question import UltimateQuestion
+from processes.centroids import Centroids
+from processes.sayhello import SayHello
+from processes.feature_count import FeatureCount
+from processes.buffer import Buffer
+from processes.area import Area
+from processes.bboxinout import Box
+from processes.jsonprocess import TestJson
+
+
+app = flask.Flask(__name__)
+
+processes = [
+    FeatureCount(),
+    SayHello(),
+    Centroids(),
+    UltimateQuestion(),
+    Sleep(),
+    Buffer(),
+    Area(),
+    Box(),
+    TestJson()
+]
+
+# For the process list on the home page
+
+process_descriptor = {}
+for process in processes:
+    abstract = process.abstract
+    identifier = process.identifier
+    process_descriptor[identifier] = abstract
+
+# This is, how you start PyWPS instance
+service = Service(processes, ['pywps.cfg'])
+
+
+@app.route("/")
+def hello():
+    server_url = pywps.configuration.get_config_value("server", "url")
+    request_url = flask.request.url
+    return flask.render_template('home.html', request_url=request_url,
+                                 server_url=server_url,
+                                 process_descriptor=process_descriptor)
+
+
+@app.route('/wps', methods=['GET', 'POST'])
+def wps():
+
+    return service
+
+
+@app.route('/outputs/'+'<filename>')
+def outputfile(filename):
+    targetfile = os.path.join('outputs', filename)
+    if os.path.isfile(targetfile):
+        file_ext = os.path.splitext(targetfile)[1]
+        with open(targetfile, mode='rb') as f:
+            file_bytes = f.read()
+        mime_type = None
+        if 'xml' in file_ext:
+            mime_type = 'text/xml'
+        return flask.Response(file_bytes, content_type=mime_type)
+    else:
+        flask.abort(404)
+
+
+@app.route('/static/'+'<filename>')
+def staticfile(filename):
+    targetfile = os.path.join('static', filename)
+    if os.path.isfile(targetfile):
+        with open(targetfile, mode='rb') as f:
+            file_bytes = f.read()
+        mime_type = None
+        return flask.Response(file_bytes, content_type=mime_type)
+    else:
+        flask.abort(404)
+
 if __name__ == "__main__":
-    pass
+    import argparse
+
+    parser = argparse.ArgumentParser(
+        description="""Script for starting PyWPS
+         demo instance with sample processes""",
+        epilog="""Do not use demo in production environment.
+         It's intended to be running in test environment only!
+        For more documentation, visit http://pywps.org/doc
+        """
+        )
+    parser.add_argument('-d', '--daemon',
+                        action='store_true', help="run in daemon mode")
+    parser.add_argument('-a','--all-addresses',
+                        action='store_true', help="run flask using IPv4 0.0.0.0 (all network interfaces),"  +  
+                            "otherwise bind to 127.0.0.1 (localhost).  This maybe necessary in systems that only run Flask") 
+    args = parser.parse_args()
+    
+    if args.all_addresses:
+        bind_host='0.0.0.0'
+    else:
+        bind_host='127.0.0.1'
+
+    if args.daemon:
+        pid = None
+        try:
+            pid = os.fork()
+        except OSError as e:
+            raise Exception("%s [%d]" % (e.strerror, e.errno))
+
+        if (pid == 0):
+            os.setsid()
+            app.run(threaded=True,host=bind_host)
+        else:
+            os._exit(0)
+    else:
+        app.run(threaded=True,host=bind_host)

90e9dd82addf0ec14b3600c271fbe0b4b110227c
Martin Lacayo-Emery <mlacayoemery@gmail.com>
2017-11-06 12:18:36 +0100

    Adapted script for Baobab setup.

--- tools/invest/test_invest_carbon_demo.py
+++ tools/invest/test_invest_carbon_demo.py
@@ -5,21 +5,28 @@ InVEST version: 3.3.3
 """
 
 import natcap.invest.carbon
-
+import os
 
 args = {
-        u'carbon_pools_path': u'D:/Martin/Data/carbon/carbon_pools_samp.csv',
+        u'carbon_pools_path': u'~/workspace/data/carbon/carbon_pools_samp.csv',
         u'discount_rate': 7.0,
         u'do_valuation': True,
-        u'lulc_cur_path': u'D:/Martin/Data/Base_Data/Terrestrial/lulc_samp_cur.tif',
+        u'lulc_cur_path': u'~/workspace/data/Base_Data/Terrestrial/lulc_samp_cur.tif',
         u'lulc_cur_year': 2016,
-        u'lulc_fut_path': u'D:/Martin/Data/Base_Data/Terrestrial/lulc_samp_fut.tif',
+        u'lulc_fut_path': u'~/workspace/data/Base_Data/Terrestrial/lulc_samp_fut.tif',
         u'lulc_fut_year': 2030,
-        u'lulc_redd_path': u'D:/Martin/Data/carbon/lulc_samp_redd.tif',
+        u'lulc_redd_path': u'~/workspace/data/carbon/lulc_samp_redd.tif',
         u'price_per_metric_ton_of_c': 43.0,
         u'rate_change': 0.0,
-        u'workspace_dir': u'C:\\Users\\lacayoem/Documents/carbon_workspace',
+        u'workspace_dir': u'/tmp/carbon_workspace',
 }
 
 if __name__ == '__main__':
+    for k in args.keys():
+        try:
+            args[k] = os.path.expanduser(args[k])
+
+        except AttributeError:
+            continue
+
     natcap.invest.carbon.execute(args)

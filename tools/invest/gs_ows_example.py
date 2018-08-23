396a6fb9dbc1a38f42d5a056ea6ee46d46037563
Martin Lacayo <mlacayoemery@gmail.com>
2018-08-23 16:53:25 +0200

    Coverting geoscripting example script into using OWS.

--- tools/invest/gs_ows_example.py
+++ tools/invest/gs_ows_example.py
@@ -6,24 +6,59 @@ import natcap.invest.sdr
 import osgeo.ogr
 import os
 
+import geoserver.catalog
+import uuid
+
+def get_cat(rest_url = "http://localhost:8080/geoserver/rest",
+            username = "admin",
+            password = "geoserver"):
+    return geoserver.catalog.Catalog(rest_url, username, password)
+
+def make_named_workspace(gs_cat = get_cat(),
+                         prefix="user"):
+
+    workspace_name = "-".join([prefix,
+                               uuid.uuid1()])
+
+    workspace_url = workspace_name
+
+    return cat.create_workspace(workspace_name, workspace_url).name
+    
+def publish_shp(shp_path,
+                shp_name = None,
+                gs_workspace = make_named_workspace(),
+                gs_cat = get_cat()):
+
+    data_store_path, _ = os.path.splitext(shp_path)
+    if shp_name is None:
+        shp_name = os.path.basename(data_store_path)
+
+    shapefile_plus_sidecars = {}
+    for key in ["shp", "shx", "prj", "dbf"]:
+        shapefile_plus_sidecars[key] = ".".join([data_store_path, key])
+
+    return cat.create_featurestore(shp_name,
+                                   workspace = gs_workspace,
+                                   data = shapefile_plus_sidecars)
+   
 data_path = "/home/mlacayo/workspace/cas/data"
 
 #http://127.0.0.1:8080/geoserver/cas/wms?service=WMS&version=1.1.0&request=GetMap&layers=cas:eto&styles=&bbox=444900.9029974863,4900407.170489954,485070.9029974863,4956627.170489954&width=548&height=768&srs=EPSG:26910&format=image%2Fgeotiff
 #http://127.0.0.1:8080/geoserver/cas/wcs?service=WCS&version=1.0.0&request=GetCoverage&coverage=cas:eto&bbox=444900.9029974863,4900407.170489954,485070.9029974863,4956627.170489954&width=548&height=768&crs=EPSG:26910&format=image%2Fgeotiff
 
 sdr_base_args = {
-    u'biophysical_table_path': u'D:/invest/input/biophysical_table.csv',
-    u'dem_path': u'D:/invest/input/dem.tif',
+    u'biophysical_table_path': u'/home/mlacayo/workspace/cas/data/biophysical_table.csv',
+    u'dem_path': u'/home/mlacayo/workspace/cas/data/dem.tif',
     u'drainage_path': u'',
-    u'erodibility_path': u'D:/invest/input/erodibility.tif',
-    u'erosivity_path': u'D:/invest/input/erosivity.tif',
+    u'erodibility_path': u'/home/mlacayo/workspace/cas/data/erodibility.tif',
+    u'erosivity_path': u'/home/mlacayo/workspace/cas/data/erosivity.tif',
     u'ic_0_param': u'0.5',
     u'k_param': u'2',
-    u'lulc_path': u'D:/invest/input/landuse_90.tif',
+    u'lulc_path': u'/home/mlacayo/workspace/cas/data/landuse_90.tif',
     u'sdr_max': u'0.8',
     u'threshold_flow_accumulation': u'1000',
-    u'watersheds_path': u'D:/invest/input/watersheds.shp',
-    u'workspace_dir': u'D:/invest/output/sdr_base',
+    u'watersheds_path': u'/home/mlacayo/workspace/cas/data/watersheds.shp',
+    u'workspace_dir': u'/home/mlacayo/workspace/cas/data/output/output/sdr_base',
     }
 
 #convert 50,000 HA (~36%) of pasture (type 80) to conifers (type 56)
@@ -31,14 +66,14 @@ sdr_base_args = {
 gen_forest_args = {
     u'aoi_path': u'',
     u'area_to_convert': u'50000',
-    u'base_lulc_path': u'D:/invest/input/landuse_90.tif',
+    u'base_lulc_path': u'/home/mlacayo/workspace/cas/data/landuse_90.tif',
     u'convert_farthest_from_edge': False,
     u'convert_nearest_to_edge': True,
     u'convertible_landcover_codes': u'80',
     u'focal_landcover_codes': u'56 57 58 59 60 61',
     u'n_fragmentation_steps': u'1',
     u'replacment_lucode': u'56',
-    u'workspace_dir': u'D:/invest/output/scenario_forest',
+    u'workspace_dir': u'/home/mlacayo/workspace/cas/data/output/output/scenario_forest',
     }
 
 #convert 50,000 HA (~36%) of pasture (type 80) to low density residential (type 1)
@@ -46,17 +81,19 @@ gen_forest_args = {
 gen_residential_args = {
     u'aoi_path': u'',
     u'area_to_convert': u'50000',
-    u'base_lulc_path': u'D:/invest/input/landuse_90.tif',
+    u'base_lulc_path': u'/home/mlacayo/workspace/cas/data/landuse_90.tif',
     u'convert_farthest_from_edge': False,
     u'convert_nearest_to_edge': True,
     u'convertible_landcover_codes': u'80',
     u'focal_landcover_codes': u'1 2 3 4',
     u'n_fragmentation_steps': u'1',
     u'replacment_lucode': u'1',
-    u'workspace_dir': u'D:/invest/output/scenario_residential',
+    u'workspace_dir': u'/home/mlacayo/workspace/cas/data/output/output/scenario_residential',
     }
 
 if __name__ == '__main__':
+    job_queue = []
+    
     ###run the SDR baseline with 3 differnt threshold flow accumulations
 
     #create a list of the test flows
@@ -157,10 +194,10 @@ if __name__ == '__main__':
     sdr_forest_args = copy.copy(sdr_base_args)
 
     #set the worspace directory
-    sdr_forest_args[u'workspace_dir'] = u'D:/invest/output/sdr_scenario_forest'
+    sdr_forest_args[u'workspace_dir'] = u'/home/mlacayo/workspace/cas/data/output/output/sdr_scenario_forest'
 
     #set the LULC to the scenario
-    sdr_forest_args[u'lulc_path'] = u'D:/invest/output/scenario_forest/nearest_to_edge.tif'
+    sdr_forest_args[u'lulc_path'] = u'/home/mlacayo/workspace/cas/data/output/output/scenario_forest/nearest_to_edge.tif'
 
     #run the SDR forest scenario
     natcap.invest.sdr.execute(sdr_forest_args)
@@ -169,10 +206,10 @@ if __name__ == '__main__':
     sdr_residential_args = copy.copy(sdr_base_args)
 
     #set the worspace directory
-    sdr_residential_args[u'workspace_dir'] = u'D:/invest/output/sdr_scenario_residential'
+    sdr_residential_args[u'workspace_dir'] = u'/home/mlacayo/workspace/cas/data/output/output/sdr_scenario_residential'
 
     #set the LULC to the scenario
-    sdr_residential_args[u'lulc_path'] = u'D:/invest/output/scenario_residential/nearest_to_edge.tif'
+    sdr_residential_args[u'lulc_path'] = u'/home/mlacayo/workspace/cas/data/output/output/scenario_residential/nearest_to_edge.tif'
 
     #run the SDR residential scenario
     natcap.invest.sdr.execute(sdr_residential_args)

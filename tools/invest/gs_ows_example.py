4376151a2f05fdd48d7fd2245f7c33321e370871
Martin Lacayo <mlcayoemery@gmail.com>
2018-09-20 17:17:43 +0200

    Added workaround for easyows import.

--- tools/invest/gs_ows_example.py
+++ tools/invest/gs_ows_example.py
@@ -1,3 +1,9 @@
+if __name__ == '__main__' and __package__ is None:
+    from os import sys, path
+    sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
+
+import easyows
+
 import copy
 
 import natcap.invest.scenario_gen_proximity

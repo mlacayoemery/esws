789b7f170b764838da3021e8c6a1c16a36d09743
Martin Lacayo <mlcayoemery@gmail.com>
2019-04-17 16:27:55 +0200

    Porting GS OWS over to new Easy OWS.

--- tools/invest/gs_ows_example.py
+++ tools/invest/gs_ows_example.py
@@ -1,3 +1,5 @@
+import logging
+
 if __name__ == '__main__' and __package__ is None:
     from os import sys, path
     p = path.dirname(path.dirname(path.abspath(__file__)))
@@ -5,6 +7,7 @@ if __name__ == '__main__' and __package__ is None:
     sys.path.append(p)
 
 import easyows
+import gdaltools
 
 import copy
 
@@ -13,206 +16,7 @@ sys.path.append("/home/mlacayo/workspace/invest/src/natcap/invest/pygeoprocessin
 import natcap.invest.scenario_gen_proximity
 import natcap.invest.sdr
 
-import osgeo.ogr
-import os
-
-import geoserver.catalog
-import uuid
-
-import csv
-
-try:
-    #Python2
-    import urllib
-    urlretrieve = urllib.URLopener().retrieve
-    
-except ImportError:
-    #Python
-    from urllib.request import urlretrieve
-
-
-import tempfile
-import zipfile
-
-gs_url = "http://localhost:8080/gs213"
-
-class MissingResource(Exception):
-    pass
-
-def get_cat(rest_url = gs_url + "/rest",
-            username = "admin",
-            password = "geoserver"):
-    return geoserver.catalog.Catalog(rest_url, username, password)
-
-def make_named_workspace(gs_cat = get_cat(),
-                         prefix="user"):
-
-    workspace_name = "-".join([prefix,
-                               str(uuid.uuid1())])
-
-    workspace_url = workspace_name
-
-    return gs_cat.create_workspace(workspace_name, workspace_url).name
-    
-def publish_shp(shp_path,
-                shp_name = None,
-                gs_workspace = make_named_workspace(),
-                gs_cat = get_cat()):
-
-    data_store_path, _ = os.path.splitext(shp_path)
-    if shp_name is None:
-        shp_name = os.path.basename(data_store_path)
-
-    shapefile_plus_sidecars = {}
-    for key in ["shp", "shx", "prj", "dbf"]:
-        shapefile_plus_sidecars[key] = ".".join([data_store_path, key])
-
-    return gs_cat.create_featurestore(shp_name,
-                                      workspace = gs_workspace,
-                                      data = shapefile_plus_sidecars)
-
-
-def publish_tif(tif_path,
-                tif_name = None,
-                gs_workspace = make_named_workspace(),
-                gs_cat = get_cat()):
-
-    data_store_path, _ = os.path.splitext(tif_path)
-    if tif_name is None:
-        tif_name = os.path.basename(data_store_path)
-
-    tiffdata = { 'tiff' : tif_path }
-
-    return gs_cat.create_coveragestore_external_geotiff(tif_name,
-                                                        "file://" + tif_path,
-                                                        gs_cat.get_workspace(gs_workspace)) 
-
-
-def extract_wrapper(args):
-    extract_shapefile_value_csv(**args)
-
-def extract_shapefile_value_csv(shapefile_path, key, csv_path, id_field="ws_id", id_value=1, value_field="sed_retent"):
-    #open the output results
-    data_source = driver.Open(shapefile_path, 0)
-
-    #get the actual data from the results
-    layer = data_source.GetLayer()
-
-    #loop over each feature in the data
-    for feature in layer:
-
-        #check if watershed 1 found
-        if feature.GetField(id_field) == 1:
-
-            #save retention
-            with open(csv_path, 'ab') as csvfile:
-                datawriter = csv.writer(csvfile)
-                datawriter.writerow([key, feature.GetField(value_field)])
-
-            data_source = None
-
-            return None
-
-
-def local_parameters(args):
-    for key, value in args.items():
-        if type(value) == str:
-            if value[:4].lower() == "http":
-                return False
-
-    return True
-
-ows_cache = {}
-
-def get_remote_parameters(args):
-    for key, value in args.items():
-        if type(value) == str:
-            if value[:4].lower() == "http":
-                #print(value)
-                if "service=WFS" in value:
-                    if value in ows_cache:
-                        args[key] = ows_cache[value]
-                        print("\t\tAssigned %s cached %s" % (key, args[key]))
-
-                    else:
-                        try:
-                            _, tmp_path = tempfile.mkstemp(suffix=".zip", prefix="esws-")
-                            urlretrieve(value, tmp_path)
-
-                            tmp_dir = tempfile.mkdtemp(prefix="esws-")
-                            zipfile.ZipFile(tmp_path, 'r').extractall(tmp_dir)
-
-                            for wfs_file in os.listdir(tmp_dir):
-                                if wfs_file.endswith(".shp"):
-                                    args[key] = os.path.join(tmp_dir,wfs_file)
-                                    print("\t\tAssigned %s %s" % (key, args[key]))
-                                    ows_cache[value] = args[key]
-                                    
-                        except zipfile.BadZipfile:
-                            print("\t\tMissing %s" % value)
-                            raise MissingResource("Missing resource")
-
-                elif "service=WCS" in value:
-                    if value in ows_cache:
-                        args[key] = ows_cache[value]
-                        print("\t\tAssigned %s cached %s" % (key, args[key]))
-
-                    else:
-                        _, tmp_path = tempfile.mkstemp(suffix=".tif", prefix="esws-")
-                        urlretrieve(value, tmp_path)
-                        args[key] = tmp_path
-                        print("\t\tAssigned %s %s" % (key, args[key]))
-                        ows_cache[value] = args[key]
-
-                else:
-                    raise ValueError("Unknown protocol for %s" % value)
-               
-def layer_url(layer_name):
-    template = gs_url + "/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=%s&outputFormat=SHAPE-ZIP"
-
-    return template % layer_name
-
-def cover_url(layer_name):
-    template = gs_url + "/ows?service=WCS&version=2.0.0&request=GetCoverage&coverageId=%s&format=image%%2Fgeotiff"
-
-    return template % layer_name
-    
-def run_job(job):
-    job[0] += 1
-    _, process, args, uploads, msg = job
-    print("Trying %s" % msg)
-    if local_parameters(args):
-        process(args)
-        for layer_name, layer_path in uploads.items():
-            print("\tUploading %s" % layer_name)
-            ws, layer_name = layer_name.split(":")
-            if layer_path.lower().endswith(".shp"):
-                publish_shp(layer_path, layer_name, ws)
-
-            elif layer_path.lower().endswith(".tif"):
-                publish_tif(layer_path, layer_name, ws)
-
-            else:
-                raise ValueError(layer_path)
-        return True
-
-    else:
-        print("\tDownloading remote inputs")
-        try:
-            get_remote_parameters(job[2])
-
-        except MissingResource:
-            pass
-
-    return False
-   
 data_path = "/home/mlacayo/workspace/cas/data"
-
-#http://127.0.0.1:8080/geoserver/cas/wms?service=WMS&version=1.1.0&request=GetMap&layers=cas:eto&styles=&bbox=444900.9029974863,4900407.170489954,485070.9029974863,4956627.170489954&width=548&height=768&srs=EPSG:26910&format=image%2Fgeotiff
-#http://127.0.0.1:8080/geoserver/cas/wcs?service=WCS&version=1.0.0&request=GetCoverage&coverage=cas:eto&bbox=444900.9029974863,4900407.170489954,485070.9029974863,4956627.170489954&width=548&height=768&crs=EPSG:26910&format=image%2Fgeotiff
-#http://127.0.0.1:8080/geoserver/ows?service=WCS&version=1.0.0&request=DescribeCoverage&coverage=cas:eto
-
-#http://127.0.0.1:8080/geoserver/cas/wcs?service=WCS&version=2.0.0&request=GetCoverage&coverageId=cas:eto&&format=image%2Fgeotiff
 
 sdr_base_args = {
     u'biophysical_table_path': u'/home/mlacayo/workspace/cas/data/biophysical_table.csv',
@@ -260,16 +64,21 @@ gen_residential_args = {
     }
 
 if __name__ == '__main__':
-    cat = get_cat()
-    print("Removing workspace(s)")
-    for ws in cat.get_workspaces():
-        if ws.name[:5] == "user-":
-            print("\t%s" % ws.name)
-            cat.delete(ws, recurse=True)
-        
+    logger = logging.getLogger('gsows')
+    
+    cat = easyows.Catalog(gs_url = "http://localhost:8080/geoserver",
+                          username = "admin",
+                          password = "geoserver",
+                          ws_prefix = "user-",
+                          logger = logger)
+
+    
+    logger.info("Removing workspace(s)")
+    cat.clean_named_workspace()
+       
     csv_path = "/home/mlacayo/workspace/cas/data/output/sed_retent.csv"
     
-    job_queue = [] #(priority, process, args, uploads, msg)
+    job_queue = []
     
     ###run the SDR baseline with 3 differnt threshold flow accumulations
 
@@ -279,9 +88,6 @@ if __name__ == '__main__':
     #set constants for outputs
     retention_field = "sed_retent"
     id_field = "ws_id"
-
-    #get driver to read shapefiles
-    driver = osgeo.ogr.GetDriverByName("ESRI Shapefile")    
 
     #create dictionary to store results
     retention_dict = {}
@@ -302,7 +108,7 @@ if __name__ == '__main__':
         #run SDR with the current parameters
         #natcap.invest.sdr.execute(args)
 
-        ws = make_named_workspace()
+        ws = cat.make_named_workspace()
 
         layer_name = ":".join([ws, "sdr"])
 
@@ -310,11 +116,13 @@ if __name__ == '__main__':
             layer_name : os.path.join(args[u'workspace_dir'], u'watershed_results_sdr.shp')
         }
         
-        job_queue.append([0,
-                          natcap.invest.sdr.execute,
-                          args,
-                          uploads,
-                          "Running SDR with flow %i" % flow])
+        job_queue.append(easyows.Job(natcap.invest.sdr.execute,
+                                     args,
+                                     uploads,
+                                     "Running SDR with flow %i" % flow,
+                                     0,
+                                     cat,
+                                     logger))
 
 
         args = {
@@ -324,11 +132,13 @@ if __name__ == '__main__':
 
         uploads = {}
 
-        job_queue.append([0,
-                          extract_wrapper,
-                          args,
-                          uploads,
-                          "Reading SDR results for flow %i" % flow])
+        job_queue.append(easyows.Job(gdaltools.extract_wrapper,
+                                     args,
+                                     uploads,
+                                     "Reading SDR results for flow %i" % flow,
+                                     0,
+                                     cat,
+                                     logger))
 
     while len(job_queue) != 0:
         job = job_queue.pop(0)

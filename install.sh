149832b0676cd1a4ba947f2a37267ed6bb1562f5
Martin Lacayo <mlacayoemery@gmail.com>
2019-04-25 17:48:32 +0200

    Updating setup files

--- install.sh
+++ install.sh
@@ -1,3 +1,20 @@
+#install system requirements
 xargs apt-get install -y < requirements.system
+
+#install GDAL with Python 2 and 3 bindings
+cd ..
+export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
+ldconfig
+wget http://download.osgeo.org/gdal/2.3.1/gdal-2.3.1.tar.gz
+tar -xvf gdal-2.3.1.tar.gz
+cd gdal-2.3.1
+./configure --with-python
+make
+make install
+pip2 install /data/share/gdal-2.3.1/swig/python
+pip3 install /data/share/gdal-2.3.1/swig/python
+cd ../esws
+
+#install Python requirements
 pip2 install -r requirements.txt
 

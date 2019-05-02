73533d715b263980bf9d20367b7185cd6c388a69
Martin Lacayo <mlcayoemery@gmail.com>
2019-05-02 09:23:34 +0200

    Updating setup files

--- install.sh
+++ install.sh
@@ -125,12 +125,13 @@ read -p "Press [Enter] key to continue..."
 ;;
 
 7)
-sudo systemctl stop esws-wps-client@esws
-sudo systemctl disable esws-wps-client@esws
-sudo cp esws-wps-client@esws.service /etc/systemd/system
-sudo systemctl reload esws-wps-client@esws
-sudo systemctl start esws-wps-client@esws
-sudo systemctl enable esws-wps-client@esws
+sudo systemctl stop esws-dashboard
+sudo systemctl disable esws-dashboard
+sudo cp esws-dashboard.service /etc/systemd/system
+sudo chmod 644 /etc/systemd/system/esws-dashboard.service
+sudo systemctl reload esws-dashboard
+sudo systemctl start esws-dashboard
+sudo systemctl enable esws-dashboard
 read -p "Press [Enter] key to continue..."
 ;;
 8)

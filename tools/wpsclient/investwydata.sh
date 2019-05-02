d4b1aa25b1e8b35552321b2800957bd9fd2b9db8
Martin Lacayo <mlcayoemery@gmail.com>
2019-05-02 12:05:57 +0200

    Updating setup files

--- tools/wpsclient/investwydata.sh
+++ tools/wpsclient/investwydata.sh
@@ -1,6 +1,6 @@
 wget "http://127.0.0.1:8000/server/CSV/register/Local CSV/url/http://127.0.0.1:8001" -O /dev/null
-wget "http://127.0.0.1:8000/server/WCS/register/Local WCS/url/http://127.0.0.1:8080/geoserver/ows" -O /dev/null
-wget "http://127.0.0.1:8000/server/WFS/register/Local WFS/url/http://127.0.0.1:8080/geoserver/ows" -O /dev/null
+wget "http://127.0.0.1:8000/server/WCS/register/Local WCS/url/http://127.0.0.1:8080/gs213/ows" -O /dev/null
+wget "http://127.0.0.1:8000/server/WFS/register/Local WFS/url/http://127.0.0.1:8080/gs213/ows" -O /dev/null
 wget "http://127.0.0.1:8000/server/WPS/register/Local WPS/url/http://127.0.0.1:5000/wps" -O /dev/null
 
 wget "http://127.0.0.1:8000/server/CSV/1/register/wy.csv" -O /dev/null

c945adc185cdda2e9826229ca9d04bb3a3e3f72a
Martin Lacayo <mlacayoemery@gmail.com>
2019-05-17 15:31:52 +0200

    Updated WY demo with new server addresses

--- tools/wpsclient/investwydata.sh
+++ tools/wpsclient/investwydata.sh
@@ -1,7 +1,7 @@
-wget "http://127.0.0.1:8000/server/CSV/register/Local CSV/url/http://127.0.0.1:8001" -O /dev/null
-wget "http://127.0.0.1:8000/server/WCS/register/Local WCS/url/http://127.0.0.1:8081/gs215/ows" -O /dev/null
-wget "http://127.0.0.1:8000/server/WFS/register/Local WFS/url/http://127.0.0.1:8081/gs215/ows" -O /dev/null
-wget "http://127.0.0.1:8000/server/WPS/register/Local WPS/url/http://127.0.0.1:5000/wps" -O /dev/null
+wget "http://127.0.0.1:8000/server/CSV/register/Local CSV/url/http://192.168.122.22:8001" -O /dev/null
+wget "http://127.0.0.1:8000/server/WCS/register/Local WCS/url/http://127.0.0.1:8080/gs215/ows" -O /dev/null
+wget "http://127.0.0.1:8000/server/WFS/register/Local WFS/url/http://127.0.0.1:8080/gs215/ows" -O /dev/null
+wget "http://127.0.0.1:8000/server/WPS/register/Local WPS/url/http://192.168.122.22:5000/wps" -O /dev/null
 
 wget "http://127.0.0.1:8000/server/CSV/1/register/wy.csv" -O /dev/null
 

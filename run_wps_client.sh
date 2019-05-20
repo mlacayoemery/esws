065f3a83fd2c380ec6fcad7f540b7ec93c9c1827
Martin Lacayo <mlcayoemery@gmail.com>
2019-05-20 16:34:23 +0200

    Fixed startup script

--- run_wps_client.sh
+++ run_wps_client.sh
@@ -1,4 +1,4 @@
 thisScriptPath=`realpath $0`
 thisDirPath=`dirname $thisScriptPath`
 
-python3 $thisDirPath/tools/wpsclient/manage.py runserver
+python3 $thisDirPath/tools/wpsclient/manage.py runserver 0.0.0.0:8000

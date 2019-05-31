9658228e2dca22d17e8278931ca00bf56e79180f
Martin Lacayo <mlacayoemery@gmail.com>
2019-05-31 10:27:09 +0200

    Fixed WPS client reset script paths in root directory

--- reset_wps_client.sh
+++ reset_wps_client.sh
@@ -1,6 +1,6 @@
 thisScriptPath=`realpath $0`
 thisDirPath=`dirname $thisScriptPath`
 
-rm $thisDirPath/db.sqlite3
-rm -rf $thisDirPath/wpsclient/migrations
-sh $thisDirPath/setup.sh
+rm $thisDirPath/tools/wpsclient/db.sqlite3
+rm -rf $thisDirPath/tools/wpsclient/wpsclient/migrations
+sh $thisDirPath/tools/wpsclient/setup.sh

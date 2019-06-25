9593caa033645861ebe08c6ebac1334c8847ce9c
Martin Lacayo <mlacayoemery@gmail.com>
2019-06-25 16:46:22 +0200

    SVG arcs

--- docs/svg_invest.py
+++ docs/svg_invest.py
@@ -1,4 +1,5 @@
 import svgwrite
+import math
 
 #https://stackoverflow.com/questions/17127083/python-svgwrite-and-font-styles-sizes
 def invest_model(path,
@@ -94,6 +95,75 @@ def connector_vector(insert=(0,0),
              stroke="#000000", stroke_width=unit/40.0)
 
 
+def eliptical_arc(start_x=0,
+                  start_y=0,
+                  stop_y=1,
+                  radius=1,
+                  start_deg=0,
+                  stop_deg=180):
+   
+    degree0 = start_deg
+    degree1 = stop_deg
+    radians0 = math.radians(degree0)
+    radians1 = math.radians(degree1)
+    dx0 = radius*(math.sin(radians0))
+    dy0 = radius*(math.cos(radians0))
+    dx1 = radius*(math.sin(radians1))
+    dy1 = radius*(math.cos(radians1))
+
+    m0 = dy0 
+    n0 = -dx0 
+    m1 = -dy0 + dy1 
+    n1 = dx0 - dx1 
+
+    p = ""
+    if start_x is None or start_y is None:
+        p = " m0,0"
+    else:
+        p = " M%f,%f" % (start_x, start_y)
+        
+    p = p + "M %f,%f" % (m0, n0)
+    
+    p = p + "a %f,%f 0 0,0 %f,%f" % (radius, radius, m1, n1)
+
+    #p = p + " z"
+
+    print(p)
+
+    return svgwrite.path.Path(d=p,
+             fill="none", 
+             stroke="#000000", stroke_width=1/40.0)
+
+def connector_misc(insert=(0,0),
+                    unit=1):
+
+##       " l0,%i l0,%i"\
+##       " l%i,0"\
+##       " l0,-%i l0,-%i l0,-%i l0,-%i l0,-%i"\
+##       " l-%i,0"\
+##       " l0,%i l0,%i"\
+
+    t="M0,0"\
+       " l0,-%i l0,-%i l0,-%i l0,-%i l0,-%i l0,-%i l0,-%i"\
+       " l%i,0 l%i,0 l%i,0 l%i,0 l%i,0"
+
+    #t = t + " a%f,%f 0 0,0 %f,%f" %  (n0, radius_c, m1, n1)
+
+    #t = t + " a0.05,0.05 0 0,0 0,-1"
+    t = t + " m0,-%i"
+
+    t = t + " l-%i,0 l-%i,0 l-%i,0 l-%i,0 l-%i,0"\
+       " l0,-%i l0,-%i l0,-%i l0,-%i l0,-%i l0,-%i"
+    p = t % tuple([unit]*t.count("%i"))
+
+
+    print(p)
+
+    return svgwrite.path.Path(d=p,
+             fill="none", 
+             stroke="#000000", stroke_width=unit/40.0)
+
+
 if __name__ == "__main__":
 
     path = "test.svg"
@@ -105,7 +175,21 @@ if __name__ == "__main__":
 
     #dwg.add(connector_table())
     #dwg.add(connector_raster())    
-    dwg.add(connector_vector())
+    #dwg.add(connector_vector())
+
+    dwg.add(eliptical_arc(start_x=None,
+                          start_y=None,
+                          stop_y=1,
+                          radius=1,
+                          start_deg=0,
+                          stop_deg=180))
+
+    dwg.add(eliptical_arc(start_x=None,
+                          start_y=None,
+                          stop_y=1,
+                          radius=1,
+                          start_deg=180,
+                          stop_deg=270))
 
     dwg.save()
 
